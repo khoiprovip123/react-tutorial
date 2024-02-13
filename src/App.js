923be35b54d0e3908e6e0c19381c7319da75003c
Khoi Hoang <lahoangkhoi.ck@gmail.com>
2024-02-13 21:06:16 +0700

    export default

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 // import * as contantAll from "./const/const";
 import { name, job } from "./const/const";
+import User from "./components/User";
 
 // console.log(contantAll);
 // console.log(contantAll.name);
 console.log("name", name, "job", job);
+
+console.log(User(1, 5));
 function App() {
   return (
     <div className="App">
-      <h1>Hello Word</h1>
+      <h1>Hello Word </h1>
     </div>
   );
 }
